d3c109bd19a8b82a0ab2bb8cf030205f0a3d9de2
Anthony Galea <44701741+anthonycgalea@users.noreply.github.com>
2025-09-25 22:55:54 -0400

    Add team match data endpoint

--- app/routes/team.py
+++ app/routes/team.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+from auth.dependencies import get_current_user
 from db.database import get_session
-from services.team import get_team_or_404
+from services.team import (
+    get_match_data_for_team_at_active_event,
+    get_team_or_404,
+)
 
 router = APIRouter(
     prefix="/teams",
@@ -13,3 +17,12 @@ router = APIRouter(
 @router.get("/{teamNumber}/info")
 async def get_team_info(teamNumber: int, session: AsyncSession = Depends(get_session)):
     return await get_team_or_404(session, teamNumber)
+
+
+@router.get("/{teamNumber}/matchData")
+async def get_team_match_data(
+    teamNumber: int,
+    user=Depends(get_current_user),
+    session: AsyncSession = Depends(get_session),
+):
+    return await get_match_data_for_team_at_active_event(session, teamNumber, user)

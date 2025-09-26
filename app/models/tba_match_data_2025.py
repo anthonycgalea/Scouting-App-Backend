35e91ca107b0db02cb61cae332d3d5410aa865de
anthonycgalea <44701741+anthonycgalea@users.noreply.github.com>
2025-09-26 14:57:54 -0400

    moved net and processor in tba data to be auto/teleop agnostic

--- app/models/tba_match_data_2025.py
+++ app/models/tba_match_data_2025.py
@@ -28,10 +28,8 @@ class TBAMatchData2025(TBAMatchData, table=True):
     tl1c: int = Field(default=0)
 
     # Net and Processor
-    aNet: int = Field(default=0)
-    tNet: int = Field(default=0)
-    aProcessor: int = Field(default=0)
-    tProcessor: int = Field(default=0)
+    net: int = Field(default=0)
+    processor: int = Field(default=0)
 
     # Endgame
     endgame: Endgame2025 = Field(default=Endgame2025.NONE)
